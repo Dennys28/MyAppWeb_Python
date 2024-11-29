bd7644844b8644f2ff96712a3ecd10cb2261a9a4
Dennys28 <66804148+Dennys28@users.noreply.github.com>
2024-11-29 12:40:28 -0500

    feat: :sparkles: Change titulo
    Cambio de titulo a app.py
    
    none

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hola Mundo - Dennys Jiménez</h1>"
+    return "<h1>Aplicacion de prueba de python</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
