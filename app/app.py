ed13384ef99ed57d481828a9e337c2f4c3762aa6
Dennys28 <66804148+Dennys28@users.noreply.github.com>
2024-12-04 11:22:22 -0500

    fix: :art: New label
    nuevo texto

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    return f"<h1>Hola, Dennys --- </h1><p>IP de la instancia: {ip_address}</p>"
+    return f"<h1>Hola, se creara el balanceador de carga</h1><p>IP de la instancia: {ip_address}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
